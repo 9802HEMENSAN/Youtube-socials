40dd016160e061bbc1e3fa5682af41bc6d53195e
9802HEMENSAN <hmahilange9802@gmail.com>
2023-07-19 09:14:29 +0530

    grid class updated

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -18,7 +18,9 @@ const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        <Sidebar 
+        selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} 
+        />
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
           Copyright © 2022 JSM Media
